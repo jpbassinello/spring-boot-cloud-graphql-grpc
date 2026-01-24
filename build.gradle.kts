a30d418c0c2f3db26b0c1e6af3059ce499a6cf1d
Joao Bassinello <jpbassinello@gmail.com>
2026-01-24 19:23:30 -0300

    spring boot 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 allprojects {
-    version = "0.0.1-SNAPSHOT"
+    version = "0.1.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,18 +38,19 @@ dependencyResolutionManagement {
             version("spring-grpc", "1.0.1")
 
             // matching proto version from spring-grpc
+            // https://repo1.maven.org/maven2/org/springframework/grpc/spring-grpc-dependencies/1.0.1/spring-grpc-dependencies-1.0.1.pom
             version("protoc-protobuf", "4.33.2")
-            version("protoc-grpc", "1.78.0")
+            version("protoc-grpc", "1.77.1")
 
             // Other dependencies
-            version("keycloak", "26.0.7")
+            version("keycloak", "26.0.8")
             version("mapstruct", "1.6.3")
             version("spi-protobuf-mapstruct", "1.49.0")
             version("shedlock", "7.5.0")
             version("testcontainers", "2.0.3")
 
             // Plugins
-            plugin("spring-boot", "org.springframework.boot").version("4.0.1")
+            plugin("spring-boot", "org.springframework.boot").version("4.0.2")
             plugin("spotbugs", "com.github.spotbugs").version("6.4.8")
             plugin("protobuf", "com.google.protobuf").version("0.9.6")
             plugin("buf", "com.parmet.buf").version("0.8.5")
